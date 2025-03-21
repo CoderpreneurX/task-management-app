d0855574468f3ad762fb7f89aa5da8deec57553a
CoderpreneurX <prabjeet.jhass@gmail.com>
2025-03-21 18:18:07 +0530

    Integrated useAuth hook to allow only authorized access

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
-import TaskList from "@/components/TaskList";
+"use client";
 
-const Home = async ({}) => {
+import TaskList from "@/components/TaskList";
+import useAuth from "@/hooks/useAuth";
+
+const Home = ({}) => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (<div className="border-2 size-12 absolute translate-x-1/2 translate-y-1/2 border-blue-500 border-r-slate-200 rounded-full animate-spin"></div>);
+  }
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <div className="h-screen">
       <h1 className="text-2xl sm:text-4xl font-black fixed w-full top-0 tracking-wide text-center pt-6">
@@ -8,7 +21,7 @@ const Home = async ({}) => {
       </h1>
 
       <div className="flex justify-center">
-        {/* <TaskList /> */}
+        <TaskList user={user} />
       </div>
     </div>
   );
