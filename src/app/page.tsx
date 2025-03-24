9aa4593a93dd4c9a462f7202f4d7e7d259e7df74
CoderpreneurX <prabjeet.jhass@gmail.com>
2025-03-24 18:27:24 +0530

    Removed Welcome to task manager heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import TaskList from "@/components/TaskList";
+import Sidebar from "@/components/ui/Sidebar";
 import useAuth from "@/hooks/useAuth";
 
 const Home = ({}) => {
   const { user, loading } = useAuth();
 
   if (loading) {
-    return (<div className="border-2 size-12 absolute translate-x-1/2 translate-y-1/2 border-blue-500 border-r-slate-200 rounded-full animate-spin"></div>);
+    return (
+      <div className="border-2 size-12 absolute translate-x-1/2 translate-y-1/2 border-blue-500 border-r-slate-200 rounded-full animate-spin"></div>
+    );
   }
 
   if (!user) {
@@ -16,9 +19,7 @@ const Home = ({}) => {
 
   return (
     <div className="h-screen">
-      <h1 className="text-2xl sm:text-4xl font-black fixed w-full top-0 tracking-wide text-center pt-6">
-        Welcome to Task Manager
-      </h1>
+      {/* <Sidebar user={user} /> */}
 
       <div className="flex justify-center">
         <TaskList user={user} />
