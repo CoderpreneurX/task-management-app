d5247d017e31bd1f656198543e4a98ee3321d019
CoderpreneurX <prabjeet.jhass@gmail.com>
2025-03-24 18:28:24 +0530

    Added logout function in useEffect

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,9 +1,24 @@
 "use client";
 
+import api from "@/utils/axiosInstance";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function LogoutPage() {
     const router = useRouter();
+
+    useEffect(() => {
+        async function logout() {
+            try {
+                await api.post('/auth/logout')
+                router.push("/")
+            } catch {
+                console.log("Error logging out...")
+            }
+        }
+
+        logout()
+    }, [router])
     
     return (
         <div className="text-center my-14 font-bold text-xl">Logout Successful, redirecting...!</div>
