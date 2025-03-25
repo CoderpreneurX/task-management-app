88dac7bf7cbd6b1b610b0557c6ecf0e02d48fdc4
CoderpreneurX <prabjeet.jhass@gmail.com>
2025-03-25 18:10:41 +0530

    Prevented authenticated users from accessing signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,27 @@
 "use client";
 
 import SignupForm from "@/components/SignupForm";
+import useAuth from "@/hooks/useAuth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function SignupPage() {
-    return (
-        <div className="h-screen grid place-content-center">
-            <SignupForm />
-        </div>
-    )
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && user) {
+      router.push("/"); // Redirect logged-in users to home
+    }
+  }, [loading, user, router]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="h-screen grid place-content-center">
+      <SignupForm />
+    </div>
+  );
 }
