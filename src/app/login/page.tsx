c9961c892e5dafbf568f3d196fd159b78290f813
CoderpreneurX <prabjeet.jhass@gmail.com>
2025-03-25 18:08:33 +0530

    Prevented authenticated users to access login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,26 @@
 "use client";
 
 import LoginForm from "@/components/LoginForm";
+import useAuth from "@/hooks/useAuth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function LoginPage() {
-    return (
-        <div className="h-screen grid place-content-center">
-            <LoginForm />
-        </div>
-    )
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && user) {
+      router.push("/"); // Redirect logged-in users to home
+    }
+  }, [loading, user, router]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div className="h-screen grid place-content-center">
+      <LoginForm />
+    </div>
+  );
 }
